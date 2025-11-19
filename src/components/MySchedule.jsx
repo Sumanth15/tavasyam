037867ae315e0d3102f1711041f96a3e908f72f6
Sumanth Koushik S D <sumanthkoushik@Sumanths-MacBook-Air.local>
2025-11-20 00:47:05 +0530

    code ninth push

--- src/components/MySchedule.jsx
+++ src/components/MySchedule.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './MySchedule.css';
 import Icon1 from '../assets/Group 107 1.png';
-import Icon2 from '../assets/Image 97.png';
+import Icon2 from '../assets/Imagelegstreach.png';
 import Icon3 from '../assets/Image 98.png';
 
 const MySchedule = () => {
